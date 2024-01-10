3802aa733174c096f7c0ea3bf6add693fc6d7a26
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-01-10 17:51:35 +0700

    Fix: fix error in console

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -20,8 +20,8 @@ const ProductPage = () => {
         <section className="px-4 sm:px-6 py-8">
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
             {products.length > 0 &&
-              products.map((product) => (
-                <CardProduct id={product.id} >
+              products.map((product, index) => (
+                <CardProduct id={product.id} key={index}>
                   <CardProduct.Header images={product.image}  />
                   <CardProduct.Body name={product.title} category={product.category} />
                   <CardProduct.Footer price={product.price} id={product.id} />
