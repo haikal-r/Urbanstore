4a94815083000bef55240910e35f1e2c3c1ee9a8
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-01-31 19:17:26 +0700

    feat(cart): create summary products

--- src/components/Fragments/Summary.jsx
+++ src/components/Fragments/Summary.jsx
@@ -1,8 +1,21 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import { Button } from "../ui/button";
+import { formatPrice } from "@/lib/utils";
 
-const Summary = ({ totalPrice }) => {
+const Summary = () => {
+  const [totalPrice, setTotalPrice] = useState(0)
+  const cart = useSelector((state) => state.cart.data);
+
+  useEffect(() => {
+    const sum = cart.reduce((acc, item) => {
+      return acc + item.qty * item.price;
+    }, 0);
+    setTotalPrice(sum);
+  }, [cart]);
+
   return (
-    <div className="sticky bottom-5 lg:right-0 lg:top-32 z-10 mt-16 rounded-lg bg-gray-100 lg:px-4 lg:py-6 p-3 lg:col-span-5 lg:mt-0 lg:p-8">
+    <div className="sticky bottom-0 lg:right-0 lg:top-32 z-10 mt-16 rounded-lg bg-gray-100 lg:px-4 lg:py-6 p-3 lg:col-span-5 lg:mt-0 lg:p-8">
       <h2 className="hidden lg:block text-lg font-medium text-gray-900">
         Order Summary
       </h2>
@@ -12,9 +25,9 @@ const Summary = ({ totalPrice }) => {
             <span className="font-medium text-gray-900">
               Order total
             </span>
-            <span className="font-bold">{totalPrice}</span>
+            <span className="font-bold">{formatPrice(totalPrice)}</span>
           </div>
-          <Button className="lg:w-full mt-6 my-auto hover:before:-translate-x-[500px] rounded-lg">
+          <Button className="lg:w-full mt-6 my-auto hover:before:-translate-x-[500px] rounded-lg" disabled={totalPrice === 0}>
             Checkout
           </Button>
         </div>
