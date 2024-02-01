0f572b963ee10e81713e7e4ddddffcadde35111b
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-02-01 21:33:50 +0700

    feat: update 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,13 +1,10 @@
-import { useRouteError } from "react-router-dom"
-
 const ErrorPage = () => {
-    const error = useRouteError();
-    return(
-        <div className="flex justify-center items-center min-h-screen flex-col">
-            <h1 className="font-bold text-3xl mb-3">Opps!</h1>
-            <p className="font-medium text-lg my-2">{error.statusText || error.message}</p>
-        </div>
-    )
-}
+  return (
+    <div className="flex justify-center  items-center min-h-screen flex-col">
+      <h1 className="font-bold text-3xl mb-3">Opps!</h1>
+      <p className="font-medium text-lg my-2"></p>
+    </div>
+  );
+};
 
 export default ErrorPage;
