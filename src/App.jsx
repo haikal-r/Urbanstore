6db44592209d81f3aa724f0c64ea035bf348bbc8
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-02-01 21:32:45 +0700

    fix: fixing layout on auth page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import Footer from "./components/Layouts/Footer";
-import Navbar from "./components/Layouts/Navbar";
-import { Route, Routes } from "react-router-dom"; 
+import Layout from "./Layout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignInPage from "./pages/sign-in";
 import ProductPage from "./pages/products";
 import ProfilePage from "./pages/profile";
@@ -12,17 +11,21 @@ import ErrorPage from "./pages/404";
 const App = () => {
   return (
     <>
-      <Navbar />
-      <Routes >
-        <Route path="/" element={<ProductPage />} errorElement={<ErrorPage />} />
-        <Route path="/sign-in" element={<SignInPage />} />
-        <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/products/category/:item" element={<ProductPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/product/:id" element={<DetailProductPage />} />
-        <Route path="/cart" element={<CartPage />} />
-      </Routes>
-      <Footer />
+      <Router>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index element={<ProductPage />} />
+            <Route path="/products" element={<ProductPage />} />
+            <Route path="/products/category/:item" element={<ProductPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/products/:id" element={<DetailProductPage />} />
+            <Route path="/cart" element={<CartPage />} />
+          </Route>
+          <Route path="/sign-in" element={<SignInPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Router>
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import {
-  BrowserRouter,
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import { Toaster } from "react-hot-toast";
+// import { BrowserRouter } from "react-router-dom";
 import "@mantine/core/styles.css";
 import App from "./App";
 
@@ -16,9 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <Toaster />
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
   </React.StrictMode>
 );
