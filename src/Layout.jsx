45bdaeb7d749feeaa022fcc003601dc16b243985
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-08-20 21:45:55 +0700

    feat: create layout page

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,23 +1,19 @@
 import { Outlet } from "react-router-dom";
-import Footer from "./components/organism/Footer";
-import Navbar from "./components/organism/Navbar";
-
-
-import { Toaster } from "react-hot-toast";
+import Footer from "./components/layouts/footer";
+import Navbar from "./components/layouts/navbar";
 
 const Layout = ({ children }) => {
   return (
     <>
-      <Navbar />
-      <Toaster />
-      <main className="flex-1">
-        <section className="grid items-center gap-8 pb-8 pt-6 md:py-8 container max-w-6xl">
-          {children}
-          <Outlet />
-        </section>
-      </main>
-      <Footer />
+    <Navbar />
+    <main className="flex-1 min-h-screen">
+      {children}
+      <Outlet />
+    </main>
+    <Footer />
+    
     </>
+    
   );
 };
 
