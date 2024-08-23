551cb5343f7a5f9c0d5238cbbb21173b248cbe93
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-08-23 20:32:08 +0700

    fix: update import env

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,3 @@
-export const API_VERSION = import.meta.env.API_VERSION
-export const API_URL = import.meta.env.API_URL
+export const API_URL = import.meta.env.VITE_API_URL
 export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID
 export const GOOGLE_CLIENT_SECRET = import.meta.env.VITE_GOOGLE_CLIENT_SECRET
