11dafffbfed3ec368d6e436b964f0922973f4f4a
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-08-26 06:55:41 +0700

    feat: update loader

--- src/components/forms/form-signin.jsx
+++ src/components/forms/form-signin.jsx
@@ -35,7 +35,7 @@ const FormLogin = () => {
       dispatch(getProfile());
       
       toast.success("Success Sign in");
-      if(isSuccess) navigate("/");
+      if(!stateIsLoading) navigate("/");
       
     },
     onError: () => {
