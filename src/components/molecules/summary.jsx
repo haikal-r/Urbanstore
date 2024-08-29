cae15cc2cdd1e10cdf1c404d93e0f891adea5854
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-08-29 14:52:22 +0700

    feat: add loader

--- src/components/molecules/summary.jsx
+++ src/components/molecules/summary.jsx
@@ -2,56 +2,61 @@ import { useCreateOrderItem } from "@/features/order/use-create-order";
 import { formatPriceIDR, navigate } from "@/lib/utils";
 import toast from "react-hot-toast";
 import { Button } from "@/components/ui/button";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Icons } from "../atoms/icons";
 
 const Summary = ({ totalPrice, refetch }) => {
+  const [isLoading, setIsLoading] = useState(false);
   const { mutate: createOrderItem } = useCreateOrderItem({
     onSuccess: (response) => {
-      refetch()
+      refetch();
       if (response.data.data.token) {
         window.snap.pay(response.data.data.token, {
           onSuccess: () => {
-            navigate('/dashboard/orders')
-            toast.success('Payment success!')
+            navigate("/dashboard/orders");
+            toast.success("Payment success!");
           },
           onPending: () => {
-            navigate('/dashboard/orders')
-            toast('Waiting your payment..')
+            navigate("/dashboard/orders");
+            toast("Waiting your payment..");
           },
           onError: () => {
-            toast.error('Payment failed, something went wrong')
+            toast.error("Payment failed, something went wrong");
           },
           onClose: () => {
-            navigate('/dashboard/orders')
-            toast.error('You have not completed the payment.')
+            navigate("/dashboard/orders");
+            toast.error("You have not completed the payment.");
           },
-        })
+        });
       }
     },
     onError: (error) => {
-      toast.error(error.response.data.message)
-    }
-  })
+      toast.error(error.response.data.message);
+    },
+  });
 
   useEffect(() => {
-    const midtransUrl = 'https://app.sandbox.midtrans.com/snap/snap.js'
+    const midtransUrl = "https://app.sandbox.midtrans.com/snap/snap.js";
 
-    let scriptTag = document.createElement('script')
-    scriptTag.src = midtransUrl
-    scriptTag.setAttribute('data-client-key', import.meta.env.VITE_MIDTRANS_CLIENT_KEY)
+    let scriptTag = document.createElement("script");
+    scriptTag.src = midtransUrl;
+    scriptTag.setAttribute(
+      "data-client-key",
+      import.meta.env.VITE_MIDTRANS_CLIENT_KEY
+    );
 
-    document.body.appendChild(scriptTag)
+    document.body.appendChild(scriptTag);
 
     return () => {
-      document.body.removeChild(scriptTag)
-    }
-  }, [])
+      document.body.removeChild(scriptTag);
+    };
+  }, []);
 
   const handleSubmit = (event) => {
-    event.preventDefault()
-    createOrderItem()
-    
-  }
+    setIsLoading(true);
+    event.preventDefault();
+    createOrderItem();
+  };
 
   return (
     <div className="sticky w-auto bottom-0 md:right-0 md:top-32 z-10 mt-16 rounded-md bg-gray-100 md:px-4 md:py-6 p-3 md:col-span-5 md:mt-0 md:p-8">
@@ -63,13 +68,21 @@ const Summary = ({ totalPrice, refetch }) => {
           <div className="flex items-center md:flex-col justify-end gap-3 md:border-t md:border-gray-200 md:pt-4">
             <div className="flex flex-col md:w-full md:flex-row md:justify-between text-sm md:text-base">
               <span className="font-medium text-gray-900">Order total</span>
-              <span className="font-bold">{formatPriceIDR(totalPrice || 0)}</span>
+              <span className="font-bold">
+                {formatPriceIDR(totalPrice || 0)}
+              </span>
             </div>
             <Button
               className="md:w-full mt-6 my-auto hover:before:-translate-x-[500px] rounded-md"
-              disabled={totalPrice === 0}
+              disabled={totalPrice === 0 || isLoading}
               type="submit"
             >
+              {isLoading && (
+                <Icons.spinner
+                  className="mr-2 size-4 animate-spin"
+                  aria-hidden="true"
+                />
+              )}
               Checkout
             </Button>
           </div>
